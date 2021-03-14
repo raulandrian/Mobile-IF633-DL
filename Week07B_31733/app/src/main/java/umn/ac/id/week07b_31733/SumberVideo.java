9fb5c4a582dc43cc0a93ebe5379dce159c523493
Raul Andrian <raulandrianr@gmail.com>
2021-03-14 11:47:54 +0700

    Galeri Video

---
+++ Week07B_31733/app/src/main/java/umn/ac/id/week07b_31733/SumberVideo.java
@@ -0,0 +1,38 @@
+package umn.ac.id.week07b_31733;
+
+import java.io.Serializable;
+
+public class SumberVideo implements Serializable {
+    private String judul;
+    private String keterangan;
+    private String videoURI;
+    public SumberVideo (String judul, String keterangan, String videoURI) {
+        this.judul = judul;
+        this.keterangan = keterangan;
+        this.videoURI = videoURI;
+    }
+
+    public String getJudul() {
+        return this.judul;
+    }
+
+    public String getKeterangan () {
+        return this.keterangan;
+    }
+
+    public String getVideoURI() {
+        return  this.videoURI;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String toString() {
+        return this.getJudul() + " => " + this.getKeterangan();
+    }
+}
